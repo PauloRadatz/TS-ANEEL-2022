8a0ebef74712006458fcfdfee2c0c282ee80595e
PauloRadatz <pradatz@epri.com>
2022-09-23 11:07:25 -0400

    Running Linha_BT

--- analytics/EnergyAllocation.py
+++ analytics/EnergyAllocation.py
@@ -20,33 +20,46 @@ def do_energy_allocation(dss: py_dss_interface.DSSDLL, energy_mwh, error_mwh=1):
 
     energy_factor = 0
 
-    for i in range(100):
+    for i in range(1000):
+        energy_mwh_power_flow = 0
 
+        # update_load(dss, energy_mwh_power_flow, energy_factor)
         update_load(dss, energy_factor)
 
         dss.text("solve")
 
-        delta_energy_mwh = calc_delta_energy(dss, energy_mwh)
+        if dss.solution_read_converged():
 
-        if i == 0:
-            print(f"Original Model's Energy: {energy_mwh - delta_energy_mwh}")
+            energy_mwh_power_flow, delta_energy_mwh = calc_delta_energy(dss, energy_mwh)
 
-        energy_factor = delta_energy_mwh / energy_mwh
+            if i == 0:
+                print(f"Original Model's Energy: {energy_mwh - delta_energy_mwh}")
 
-        dss.meters_reset()
+            energy_factor = delta_energy_mwh / energy_mwh
 
-        if abs(delta_energy_mwh) < error_mwh:
-            break
+            dss.meters_reset()
+
+            if abs(delta_energy_mwh) < error_mwh:
+                break
+        else:
+            print("Problema")
 
 def update_load(dss, energy_factor):
     dss.loads_first()
     for _ in range(dss.loads_count()):
-        dss.loads_write_kw(dss.loads_read_kw() * (1 + energy_factor))
+        dss.loads_write_kw(float(dss.loads_read_kw() * (1 + energy_factor)))
         dss.loads_next()
+
+# def update_load(dss, energy_mwh_power_flow, energy_factor):
+#
+#     if energy_mwh_power_flow:
+#
+#
+#         dss.text(f"set loadmult={energy_mwh_power_flow  * (1 + energy_factor)}")
 
 def calc_delta_energy(dss, energy_mwh):
     dss.meters_first()
     energy_mwh_power_flow = dss.meters_register_values()[0]
     delta_energy_mwh = energy_mwh - energy_mwh_power_flow
 
-    return delta_energy_mwh
+    return energy_mwh_power_flow, delta_energy_mwh
